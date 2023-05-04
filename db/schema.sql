2bb59f6dc7dc3104d0f7e6b5d67025dd55b494cb
Tjcarroll1 <tjcarroll1@icloud.com>
2023-05-05 04:52:46 +1000

    trying to fix the code to run my db

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
@@ -12,20 +12,22 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
-  department_id INT NOT NULL,
+  department VARCHAR(30) NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_department 
   FOREIGN KEY (department)
   REFERENCES department(id)
-  ON DELETE SET NULL
+  ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  FOREIGN KEY (role)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
-);
+-- CREATE TABLE employee (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   role_id INT NOT NULL,
+--   manager_id INT,
+--   FOREIGN KEY (role)
+--   REFERENCES role(id)
+--   ON DELETE SET NULL,
+-- );
 
---
+++ index.js
@@ -0,0 +1,73 @@
+//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateDb = require("./utils/generateDb.js")
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role"
+    ]
+  }, 
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data, (err) => {
+        err ? console.log(err) : console.log("successful readme")
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            return writeToFile("README.md", generateMarkdown(answers))
+        })
+}
+
+// Function call to initialize app
+init();
+
+//what are the different types of prompt from iquirer
