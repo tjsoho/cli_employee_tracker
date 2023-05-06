edc3f7df0e68926602aa9ae488fc249067a04c3a
Tjcarroll1 <tjcarroll1@icloud.com>
2023-05-06 13:31:05 +1000

    removed unused files

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,7 +11,7 @@ VALUES ("CEO", 100000, 1),
         ("CTO", 100000, 4);
        
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, NULL),
+VALUES ("John", "Frank", 1, NULL),
         ("Jane", "Doe", 2, 1),
-        ("Jack", "Doe", 3, 2),
-        ("Jill", "Doe", 4, 2);
+        ("Jack", "Mi", 3, 2),
+        ("Jill", "Si", 4, 2);
